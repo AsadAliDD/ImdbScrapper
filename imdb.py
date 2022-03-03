30eeaf03c9cd4aa96da6b5d6ee58472c6932f979
AsadAliDD <asadalisps@gmail.com>
2022-03-03 23:22:44 +0100

    Added Episodes Data

--- imdb.py
+++ imdb.py
@@ -11,53 +11,53 @@ SEASON_URL='https://www.imdb.com/title/{}/episodes?season={}'
 
 
 
-def scrape_main_page(browser,content_id):
+def scrape_main_page(content_id):
 
 
     url=URL+content_id+'/?ref_=tt_ov_inf'
 
-    e = Extractor.from_yaml_file('./imdb_main_nnn.yml')
+    main = Extractor.from_yaml_file('./imdb_main.yml')
+    seasons_e=Extractor.from_yaml_file('./imdb_seasons.yml')
+    episodes_e=Extractor.from_yaml_file('./imdb_episodes.yml')
 
-    # Download the page using requests
+
+
+    # getting MainPage Info for Content 
     r = requests.get(url)
-    # Pass the HTML of the page and create 
-    data = e.extract(r.text)
-    # Print the data 
-    print(data)
+    data_main = main.extract(r.text)
+    
+    print(data_main)
 
 
     
-    url="https://www.imdb.com"+data['Episodes']
-    r=requests.get(url)
 
 
-    seasons_e=Extractor.from_yaml_file('./imdb_seasons.yml')
+
+    # Getting Number of Seasons
+    url="https://www.imdb.com"+data_main['Episodes']
+    r=requests.get(url)    
     data=seasons_e.extract(r.text)
-
-
     seasons=data['ss'].split()
 
 
     print (data)
     print (seasons)
-
+    
 
     for season in seasons:
-        episodes_url=SEASON_URL.format(content_id,season)
+        season_url=SEASON_URL.format(content_id,season)
+
+
+        r=requests.get(season_url)
+        data_episodes=episodes_e.extract(r.text)
+        print (data_episodes)
 
         
 
 
-    # browser.get(url)
-    # time.sleep(2)
-    
-    # title=browser.find_elements(By.XPATH,'//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1/text()')
-    # # title=browser.find_elements(By.XPATH,"//*[contains(text(),'Original title')]/text()")
-    # print (title)
 
 
 
 
 
-# browser=random_browser()
-scrape_main_page("browser",'tt0386676')
+scrape_main_page('tt0386676')
